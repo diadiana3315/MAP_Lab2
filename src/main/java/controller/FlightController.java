6500f9c89de9880df2f5e99718b89bd3e952186c
Dudu723 <66259894+Dudu723@users.noreply.github.com>
2023-12-04 19:43:44 +0200

    Update FlightController.java

--- src/main/java/controller/FlightController.java
+++ src/main/java/controller/FlightController.java
@@ -4,41 +4,42 @@ import domain.Flight;
 import repository.FlightRepository;
 import repository.Repository;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class FlightController {
-    private Repository<Flight> flightRepository;
+    private FlightRepository flightRepository;
 
     public FlightController(FlightRepository flightRepository) {
         this.flightRepository = flightRepository;
     }
 
-    public void saveFlight(Flight flight) {
-        flightRepository.save(flight);
+    public void saveFlight(Flight flight) throws SQLException {
+        flightRepository.addFlight(flight);
     }
 
-    public void removeFlight(int flightId) {
-        flightRepository.removeById(flightId);
+    public void removeFlight(int flightId) throws SQLException {
+        flightRepository.deleteFlight(flightId);
     }
 
-    public List<Flight> getAllFlights() {
-        return flightRepository.getAll();
+    public void getAllFlights() throws SQLException {
+        flightRepository.viewFlights();
     }
 
-    public void updateFlight(int flightId, Flight updatedFlight) {
-        Flight existingFlight = flightRepository.getById(flightId);
-
-        if (existingFlight != null) {
-            flightRepository.remove(existingFlight);
-            flightRepository.save(updatedFlight);
-        } else {
-            System.out.println("Flight not found.");
-        }
-    }
-
-    public Flight getFlightById(int flightId) {
-        Flight flight = flightRepository.getById(flightId);
-        return flight;
-    }
+//    public void updateFlight(int flightId, Flight updatedFlight) {
+//        Flight existingFlight = flightRepository.getById(flightId);
+//
+//        if (existingFlight != null) {
+//            flightRepository.remove(existingFlight);
+//            flightRepository.save(updatedFlight);
+//        } else {
+//            System.out.println("Flight not found.");
+//        }
+//    }
+//
+//    public Flight getFlightById(int flightId) {
+//        Flight flight = flightRepository.getById(flightId);
+//        return flight;
+//    }
 }
