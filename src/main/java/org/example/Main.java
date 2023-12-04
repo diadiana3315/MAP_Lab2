7db22daec2fe5d9bdb4dcf6c1359d2697dd533b8
Dudu723 <66259894+Dudu723@users.noreply.github.com>
2023-12-04 19:55:50 +0200

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -17,48 +17,20 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Main {
-    public static void main(String[] args) {
-//        FlightRepository flightRepository = new FlightRepository();
-//        AirlineRepository airlineRepository = new AirlineRepository();
-//        TicketRepository ticketRepository = new TicketRepository();
-//        AirTrafficControlRepository airTrafficControlRepository = new AirTrafficControlRepository();
-//
-//        FlightController flightController = new FlightController(flightRepository);
-//        AirlineController airlineController = new AirlineController(airlineRepository);
-//        TicketController ticketController = new TicketController(ticketRepository);
-//        AirTrafficControlController airTrafficControlController = new AirTrafficControlController(airTrafficControlRepository);
-//
-//
-//        UI ui = new UI(airTrafficControlController, flightController, airlineController, ticketController);
-//        ui.start();
+    public static void main(String[] args) throws SQLException {
+        FlightRepository flightRepository = new FlightRepository();
+        AirlineRepository airlineRepository = new AirlineRepository();
+        TicketRepository ticketRepository = new TicketRepository();
+        AirTrafficControlRepository airTrafficControlRepository = new AirTrafficControlRepository();
 
-        String url = "jdbc:sqlserver://DESKTOP-S0UCO99\\SQLEXPRESS:1433;databaseName=Airport;encrypt=true;trustServerCertificate=true";
-        String user = "JavaProject";
-        String password = "java123";
-
-        try (Connection connection = DriverManager.getConnection(url, user, password)) {
-
-            System.out.println("Connected to server");
-
-            String query = "SELECT * FROM Pilots"; 
-
-            try (Statement statement = connection.createStatement();
-                 ResultSet resultSet = statement.executeQuery(query)) {
-
-                while (resultSet.next()) {
-                    int id = resultSet.getInt("id"); 
-                    String name = resultSet.getString("name"); 
-
-                    System.out.println("ID: " + id + ", Name: " + name);
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
+        FlightController flightController = new FlightController(flightRepository);
+        AirlineController airlineController = new AirlineController(airlineRepository);
+        TicketController ticketController = new TicketController(ticketRepository);
+        AirTrafficControlController airTrafficControlController = new AirTrafficControlController(airTrafficControlRepository);
 
 
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        UI ui = new UI(airTrafficControlController, flightController, airlineController, ticketController);
+        ui.start();
 
     }
 }
