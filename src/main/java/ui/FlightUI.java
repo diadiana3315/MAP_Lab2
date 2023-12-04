fa177f3a8570f6a1465a6f7d6f63209d195b633d
Dudu723 <66259894+Dudu723@users.noreply.github.com>
2023-12-04 19:53:43 +0200

    Update FlightUI.java

--- src/main/java/ui/FlightUI.java
+++ src/main/java/ui/FlightUI.java
@@ -4,6 +4,7 @@ import controller.FlightController;
 import domain.Flight;
 import domain.Passenger;
 
+import java.sql.SQLException;
 import java.util.Scanner;
 
 public class FlightUI {
@@ -13,7 +14,7 @@ public class FlightUI {
         this.flightController = flightController;
     }
 
-    public void handleFlightOperations() {
+    public void handleFlightOperations() throws SQLException {
         int option;
         boolean exit = true;
         Scanner scanner = new Scanner(System.in);
@@ -60,55 +61,55 @@ public class FlightUI {
 
                 case 3:
                     System.out.println("Viewing all flights:");
-                    System.out.println(flightController.getAllFlights());
+                    flightController.getAllFlights();
                     break;
-                case 4:
-                    System.out.println("Updating a flight:");
-                    System.out.print("Enter Flight ID to update: ");
-                    int flightIdToUpdate = scanner.nextInt();
-                    scanner.nextLine();
-
-                    Flight existingFlight = flightController.getFlightById(flightIdToUpdate);
-
-                    if (existingFlight != null) {
-                        System.out.println("Existing Flight Details: " + existingFlight);
-
-                        System.out.print("Enter new Airline: ");
-                        String newAirline = scanner.nextLine();
-                        System.out.print("Enter new Departure Time: ");
-                        String newDepartureTime = scanner.nextLine();
-                        System.out.print("Enter new Arrival Time: ");
-                        String newArrivalTime = scanner.nextLine();
-
-                        Flight updatedFlight = new Flight(flightIdToUpdate, newAirline, newDepartureTime, newArrivalTime);
-
-                        flightController.updateFlight(flightIdToUpdate, updatedFlight);
-
-                        System.out.println("Enter passenger details:");
-                        System.out.print("First Name: ");
-                        String firstName = scanner.nextLine();
-                        System.out.print("Last Name: ");
-                        String lastName = scanner.next();
-                        System.out.print("Age: ");
-                        int age = scanner.nextInt();
-                        scanner.nextLine();
-                        System.out.print("Gender: ");
-                        String gender = scanner.nextLine();
-                        System.out.print("Passenger ID: ");
-                        int passengerId = scanner.nextInt();
-                        scanner.nextLine();
-                        System.out.print("Passport Number: ");
-                        int passportNumber = scanner.nextInt();
-                        scanner.nextLine();
-
-                        Passenger passenger = new Passenger(firstName, lastName, age, gender, passengerId, passportNumber);
-                        existingFlight.addObserver(passenger);
-                        existingFlight.notifyObservers();
-
-                    } else {
-                        System.out.println("Flight not found.");
-                    }
-                    break;
+//                case 4:
+//                    System.out.println("Updating a flight:");
+//                    System.out.print("Enter Flight ID to update: ");
+//                    int flightIdToUpdate = scanner.nextInt();
+//                    scanner.nextLine();
+//
+//                    Flight existingFlight = flightController.getFlightById(flightIdToUpdate);
+//
+//                    if (existingFlight != null) {
+//                        System.out.println("Existing Flight Details: " + existingFlight);
+//
+//                        System.out.print("Enter new Airline: ");
+//                        String newAirline = scanner.nextLine();
+//                        System.out.print("Enter new Departure Time: ");
+//                        String newDepartureTime = scanner.nextLine();
+//                        System.out.print("Enter new Arrival Time: ");
+//                        String newArrivalTime = scanner.nextLine();
+//
+//                        Flight updatedFlight = new Flight(flightIdToUpdate, newAirline, newDepartureTime, newArrivalTime);
+//
+//                        flightController.updateFlight(flightIdToUpdate, updatedFlight);
+//
+//                        System.out.println("Enter passenger details:");
+//                        System.out.print("First Name: ");
+//                        String firstName = scanner.nextLine();
+//                        System.out.print("Last Name: ");
+//                        String lastName = scanner.next();
+//                        System.out.print("Age: ");
+//                        int age = scanner.nextInt();
+//                        scanner.nextLine();
+//                        System.out.print("Gender: ");
+//                        String gender = scanner.nextLine();
+//                        System.out.print("Passenger ID: ");
+//                        int passengerId = scanner.nextInt();
+//                        scanner.nextLine();
+//                        System.out.print("Passport Number: ");
+//                        int passportNumber = scanner.nextInt();
+//                        scanner.nextLine();
+//
+//                        Passenger passenger = new Passenger(firstName, lastName, age, gender, passengerId, passportNumber);
+//                        existingFlight.addObserver(passenger);
+//                        existingFlight.notifyObservers();
+//
+//                    } else {
+//                        System.out.println("Flight not found.");
+//                    }
+//                    break;
 
                 case 0:
                     exit = false;
