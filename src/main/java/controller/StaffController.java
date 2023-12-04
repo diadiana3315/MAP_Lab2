e8976a4ef045473c9a74e1d61023083d8d98f198
Dudu723 <66259894+Dudu723@users.noreply.github.com>
2023-12-04 19:45:20 +0200

    Update StaffController.java

--- src/main/java/controller/StaffController.java
+++ src/main/java/controller/StaffController.java
@@ -2,33 +2,28 @@ package controller;
 
 import domain.Staff;
 import repository.Repository;
+import repository.StaffRepository;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public class StaffController {
-    private Repository<Staff> staffList;
+    private StaffRepository staffList;
 
-    public StaffController(Repository<Staff> staffList) {
+    public StaffController(StaffRepository staffList) {
         this.staffList = staffList;
     }
 
-    public void addStaff(Staff staff) {
-        staffList.save(staff);
+    public void addStaff(Staff staff) throws SQLException {
+        staffList.addStaffMember(staff);
     }
 
-    public void removeStaff(int staffId) {
-        List<Staff> staffPerson = staffList.getAll();
-        for (Staff staff : staffPerson) {
-            if (staff.getStaffId() == staffId) {
-                staffList.remove(staff);
-                return;
-            }
-        }
-        System.out.println("Staff member not found.");
+    public void removeStaff(int staffId) throws SQLException {
+        staffList.deleteStaffMember(staffId);
     }
 
-    public List<Staff> getAllStaff() {
-        return staffList.getAll();
+    public void getAllStaff() throws SQLException {
+        staffList.viewStaffMembers();
     }
 
 
