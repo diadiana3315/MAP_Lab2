bcc6334526c20bb09b16d4e5b82a8ebda718d7b3
Diana Cracea <118303596+diadiana3315@users.noreply.github.com>
2023-12-04 17:33:17 +0200

    Update AirplaneRepository.java

--- src/main/java/repository/AirplaneRepository.java
+++ src/main/java/repository/AirplaneRepository.java
@@ -32,15 +32,10 @@ public class AirplaneRepository extends Repository<Airplane> {
                 int airplaneId = resultSet.getInt("airplaneId");
                 String manufacturer = resultSet.getString("manufacturer");
                 String model = resultSet.getString("model");
-                int pilotId = resultSet.getInt("pilotId");
 
-                Pilot pilot = getPilotById(pilotId);
+                Airplane airplane = new Airplane(airplaneId, manufacturer, model);
 
-
-                Airplane airplane = new Airplane(airplaneId, manufacturer, model, pilot);
-
-                System.out.println("Airplane ID: " + airplane.getAirplaneId() + ", Manufacturer: " + airplane.getManufacturer() + ", Model: " + airplane.getModel() +
-                        ", Pilot: " + pilot.getPilotId());
+                System.out.println("Airplane ID: " + airplane.getAirplaneId() + ", Manufacturer: " + airplane.getManufacturer() + ", Model: " + airplane.getModel());
             }
         } catch (SQLException e) {
             e.printStackTrace();
@@ -55,7 +50,6 @@ public class AirplaneRepository extends Repository<Airplane> {
             preparedStatement.setInt(1, airplane.getAirplaneId());
             preparedStatement.setString(2, airplane.getManufacturer());
             preparedStatement.setString(3, airplane.getModel());
-            preparedStatement.setInt(4, airplane.getPilotId());
             preparedStatement.executeUpdate();
 
         } catch (SQLException e) {
@@ -82,30 +76,7 @@ public class AirplaneRepository extends Repository<Airplane> {
         }
     }
 
-    private Pilot getPilotById(int pilotId){
-        try {
-            String query = "SELECT * FROM Pilots WHERE pilotId = ?";
-            try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
-                preparedStatement.setInt(1, pilotId);
 
-                try (ResultSet resultSet = preparedStatement.executeQuery()) {
-                    if (resultSet.next()) {
-                        String firstName = resultSet.getString("firstName");
-                        String lastName = resultSet.getString("lastName");
-                        int age = resultSet.getInt("age");
-                        String gender = resultSet.getString("gender");
-                        int yearsOfActivity = resultSet.getInt("yearsOfActivity");
-
-                        return new Pilot(firstName, lastName, age, gender, pilotId, yearsOfActivity);
-                    }
-                }
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return null;
-        }
-        return null;
-    }
     private void closeConnection() {
         try {
             if (connection != null) {
